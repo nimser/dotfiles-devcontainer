c49e4c619603c818e4c85835f8ecca2817df4fd1
Nicolas Wormser <nicolas@codewithflair.org>
2025-03-10 17:25:43 +0100

    Add basic config.nix

---
+++ config.nix
@@ -0,0 +1,22 @@
+{
+  packageOverrides = pkgs: with pkgs; {
+    myPackages = pkgs.buildEnv {
+      name = "nimser-tools";
+      paths = [
+        # --- base
+        fish
+        neovim
+        nodejs_22
+        fd
+        ripgrep
+        # --- Version control
+        lazygit
+        # -- Kubernetes related
+        kubectl
+        kubectx
+        k9s
+        fluxcd
+      ];
+    };
+  };
+}
