91d6310fe5ae58f965ab96f903540a4491fd1da2
Nicolas Wormser <nicolas@codewithflair.org>
2025-03-11 20:42:53 +0100

    Basic setup script, packages chosen based on system

--- config.nix
+++ config.nix
@@ -1,21 +1,48 @@
-{
-  packageOverrides = pkgs: with pkgs; {
+
+{ pkgs ? import <nixpkgs> {} }:
+let
+  systemType = builtins.getEnv "SYSTEM_TYPE";
+
+  # remote pagkages 
+  github-neovim = builtins.fetchGit {
+    url = "https://github.com/neovim/neovim.git";
+    ref = "refs/tags/nightly";
+  };
+
+  base = with pkgs; [
+    tealdeer
+    gnumake
+    gcc
+    netplan
+    fish
+    tree
+    htop
+    nodejs_22
+    fd
+    ripgrep
+    luarocks
+    github-neovim
+  ];
+  server = with pkgs; [
+    kubectl
+    kubectx
+    k9s
+    fluxcd
+  ];
+  desktop = with pkgs; [
+    appimage-run
+    lazygit
+    discord
+    obsidian
+    brave
+  ];
+in {
+  packageOverrides = pkgs: {
     myPackages = pkgs.buildEnv {
       name = "nimser-tools";
-      paths = [
-        # --- base
-        fish
-        nodejs_22
-        fd
-        ripgrep
-        # --- Version control
-        lazygit
-        # -- Kubernetes related
-        kubectl
-        kubectx
-        k9s
-        fluxcd
-      ];
+      paths = base ++ (if systemType == "server" then server else desktop);
     };
   };
 }
+
+#git tree htop luarocks ripgrep fd-find
