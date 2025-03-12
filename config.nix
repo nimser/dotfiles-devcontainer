199dd55fe36244566151d7c4eeacca9297cb258a
Nicolas Wormser <nicolas@codewithflair.org>
2025-03-12 16:22:59 +0100

    Nix: Propagate unfree packages setting

--- config.nix
+++ config.nix
@@ -1,43 +1,48 @@
-let
-  systemType = builtins.getEnv "SYSTEM_TYPE";
-  # Create a new pkgs instance with unfree allowed
-  unfree-pkgs = import <nixpkgs> {
-    config = {
-      allowUnfree = true;
-    };
-  };
+{
+  allowUnfree = true;  # This enables unfree packages globally
 
-  base = with unfree-pkgs; [
-    tealdeer
-    gnumake
-    gcc
-    netplan
-    fish
-    tree
-    htop
-    nodejs_22
-    fd
-    ripgrep
-    luarocks
-  ];
-  server = with unfree-pkgs; [
-    kubectl
-    kubectx
-    k9s
-    fluxcd
-  ];
-  desktop = with unfree-pkgs; [
-    appimage-run
-    lazygit
-    discord
-    obsidian
-    brave
-  ];
-in {
-  packageOverrides = pkgs: {
-    myPackages = pkgs.buildEnv {
-      name = "nimser-tools";
-      paths = base ++ (if systemType == "desktop" then desktop else server);
+  packageOverrides = pkgs: 
+    let
+      systemType = builtins.getEnv "SYSTEM_TYPE";
+
+      base = with pkgs; [
+        tealdeer
+        gnumake
+        gcc
+        netplan
+        fish
+        tree
+        htop
+        nodejs_22
+        fd
+        ripgrep
+        luarocks
+      ];
+      server = with pkgs; [
+        kubectl
+        kubectx
+        k9s
+        fluxcd
+      ];
+      desktop = with pkgs; [
+        sqlite
+        gs
+        tectonic
+        mermaid-cli
+        stylua
+        appimage-run
+        fzf
+        tree-sitter
+        imagemagick
+        lazygit
+        discord
+        obsidian
+        brave
+      ];
+    in {
+      myPackages = pkgs.buildEnv {
+        name = "nimser-tools";
+        paths = base ++ (if systemType == "desktop" then desktop else server);
+      };
     };
-  };
 }
