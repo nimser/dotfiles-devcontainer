c64854a1cba66dbbafa04ff51b40fae714975507
Nicolas Wormser <gh@nwo.pm>
2025-03-27 20:53:07 +0100

    Correct fish config PATH setting

--- fish/config.fish
+++ fish/config.fish
@@ -1,18 +1,18 @@
 if status is-interactive
-  # add ~/.local/bin to path
   if test -d ~/.local/bin
     set -gx fish_user_paths $fish_user_paths ~/.local/bin
   end
 
-  # add ~/.local/distrobox/bin to path
   if test -d ~/.local/distrobox/bin
     set -gx fish_user_paths $fish_user_paths ~/.local/distrobox/bin
   end
-end
 
-# pnpm
-set -gx PNPM_HOME "/home/owner/.local/share/pnpm"
-if not string match -q -- $PNPM_HOME $PATH
-  set -gx PATH "$PNPM_HOME" $PATH
+  set -gx PNPM_HOME "/home/owner/.local/share/pnpm"
+  if not string match -q -- $PNPM_HOME $PATH
+    set -gx PATH "$PNPM_HOME" $PATH
+  end
+
+  if test -d ~/.rd/bin
+    set --export --prepend PATH "~/.rd/bin"
+  end
 end
-# pnpm end
