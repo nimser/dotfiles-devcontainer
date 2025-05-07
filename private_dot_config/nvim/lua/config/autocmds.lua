4e01d1eb42e18583b6449976ae3a84d19e1f8cc9
Nicolas Wormser <gh@nwo.pm>
2025-05-07 15:34:12 +0200

    Manage file permissions with chezmoi

---
+++ private_dot_config/nvim/lua/config/autocmds.lua
@@ -0,0 +1,17 @@
+-- Autocmds are automatically loaded on the VeryLazy event
+-- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+--
+-- Add any additional autocmds here
+-- with `vim.api.nvim_create_autocmd`
+--
+-- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
+-- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Set relativenumber but preserve absolute numbers in insert mode
+vim.cmd([[
+augroup numbertoggle
+  autocmd!
+  autocmd BufEnter,FocusGained,InsertLeave * set relativenumber
+  autocmd BufLeave,FocusLost,InsertEnter   * set norelativenumber
+augroup END
+]])
