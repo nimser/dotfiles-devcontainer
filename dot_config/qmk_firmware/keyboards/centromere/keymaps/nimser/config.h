7a869070769f7f234869b12fb1460ea24adf1f6c
Nicolas Wormser <gh@nwo.pm>
2025-05-07 15:34:12 +0200

    Manage file permissions with chezmoi

--- dot_config/qmk_firmware/keyboards/centromere/keymaps/nimser/config.h
+++
@@ -1,5 +0,0 @@
-#define ONESHOT_TAP_TOGGLE 3  /* Tapping this number of times holds the key until tapped once again. */
-#define ONESHOT_TIMEOUT 1000  // timeout during which the oneshot action can be performed
-
-#define TAPPING_TOGGLE 4 // for the TT feature. Number of taps to go in and out of the mode
-#define TAPPING_FORCE_HOLD_PER_KEY // makes it possible to use a dual role key as modifier shortly after having been tapped
