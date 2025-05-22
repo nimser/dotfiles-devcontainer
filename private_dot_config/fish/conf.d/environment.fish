899e5f20c56f0c93d0081edc10b8ac17e077dbc3
Nicolas Wormser <gh@nwo.pm>
2025-05-22 14:28:06 +0000

    Set SHELL to help `chezmoi cd` infer proper shell

--- private_dot_config/fish/conf.d/environment.fish
+++ private_dot_config/fish/conf.d/environment.fish
@@ -1,6 +1,7 @@
 # note: more env-vars are defined in other `conf.d/*` fish configuration files. use `printenv` or the fish `set -S` command to see all vars.
 set -gx RIPGREP_CONFIG_PATH "$XDG_CONFIG_HOME/ripgrep/config"
 set -gx XDG_CONFIG_HOME "$HOME/.config"
+set -gx SHELL "fish"
 set -gx TERMINAL "alacritty"
 set -gx EDITOR "nvim"
 set -gx DIFFPROG "nvim -d"
