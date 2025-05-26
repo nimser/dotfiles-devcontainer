51d3f533e3e05be8c88edf102b17b1451cd8af2a
Nicolas Wormser <gh@nwo.pm>
2025-05-27 00:14:28 +0200

    Make more PATH values global

--- private_dot_config/fish/config.fish
+++ private_dot_config/fish/config.fish
@@ -1,23 +1,15 @@
 if status is-interactive
-  if test -d ~/.local/bin
-    set -gx fish_user_paths $fish_user_paths ~/.local/bin
-  end
-
-  if test -d ~/.local/distrobox/bin
-    set -gx fish_user_paths $fish_user_paths ~/.local/distrobox/bin
-  end
-
-  set -gx PNPM_HOME "/home/owner/.local/share/pnpm"
-  if not string match -q -- $PNPM_HOME $PATH
-    set -gx PATH "$PNPM_HOME" $PATH
-  end
+ 
+  # Note: use ~/.xsessionrc instead for env variables that need to be globally available outside shell 
 
   if test -d ~/.rd/bin
-    set -gx --prepend fish_user_paths "$HOME/.rd/bin"
+    set -gx fish_user_paths "$HOME/.rd/bin"
   end
+
   if type -q mise
     mise activate fish | source
   end
+
   if type -q direnv
     direnv hook fish | source
   end
