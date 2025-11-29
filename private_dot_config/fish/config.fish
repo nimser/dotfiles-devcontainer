0c6a587e836ae1e0ad2e3d08b7c0ea8efca1aa6c
Nicolas Wormser <gh@nwo.pm>
2025-11-29 23:05:23 +0100

    Fix auto GPG decryption not working

--- private_dot_config/fish/config.fish
+++ private_dot_config/fish/config.fish
@@ -1,6 +1,21 @@
 if status is-interactive
- 
+
   # Note: use ~/.xsessionrc instead for env variables that need to be globally available outside shell 
+
+  # First login: decrypt encrypted chezmoi files
+  if type -q chezmoi
+    if command -v gpg > /dev/null; and gpg --list-secret-keys > /dev/null 2>&1
+      if not test -f "$HOME/.config/fish/conf.d/secret.environment.fish"
+        set_color green
+        echo ">> First connection detected: Decrypting chezmoi secrets..."
+        set_color normal
+        chezmoi apply --include=encrypted
+        set_color green
+        echo ">> Secrets decrypted successfully."
+        set_color normal
+      end
+    end
+  end
 
   if test -d ~/.rd/bin
     set -gx fish_user_paths "$HOME/.rd/bin"
