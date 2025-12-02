7a662966e5dffc7a2b74cdf11f922af9a125dd8a
Nicolas Wormser <gh@nwo.pm>
2025-12-02 01:21:34 +0100

    feat(nvim): customize builtin signature_help + change colortheme\ gruvbox-material theme is broken for modern features like current parameter highlighting, reverting to catpuccin

--- private_dot_config/nvim/lua/plugins/colorscheme.lua
+++ private_dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,15 @@
 return {
   -- add gruvbox
-  { "sainnhe/gruvbox-material" },
+  -- { "sainnhe/gruvbox-material" },
+  -- { "ellisonleao/gruvbox.nvim" }, -- too vivid but good features
+  { "catppuccin/nvim" },
+  -- { "folke/tokyonight.nvim"},
 
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox-material",
+      colorscheme = "catppuccin-macchiato",
     },
   },
 }
