3d5d1cf67b2b0737838545e98fb51b20c18370be
Nicolas Wormser <gh@nwo.pm>
2025-12-22 21:18:07 +0100

    nvim theme update, local bin PATH prepend, misc...

--- private_dot_config/nvim/lua/plugins/colorscheme.lua
+++ private_dot_config/nvim/lua/plugins/colorscheme.lua
@@ -2,14 +2,17 @@ return {
   -- add gruvbox
   -- { "sainnhe/gruvbox-material" },
   -- { "ellisonleao/gruvbox.nvim" }, -- too vivid but good features
+  { "f4z3r/gruvbox-material.nvim" },
+  { "ellisonleao/gruvbox.nvim" },
   { "catppuccin/nvim" },
-  -- { "folke/tokyonight.nvim"},
+  { "folke/tokyonight.nvim" },
+  { "neanias/everforest-nvim" },
 
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin-macchiato",
+      colorscheme = "tokyonight-moon",
     },
   },
 }
