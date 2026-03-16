069cac0fd82e2c21ddf1ae3f13eeaef6cdc0414d
Nicolas Wormser <gh@nwo.pm>
2026-03-16 14:05:57 +0000

    Load secrets in ways that makes them available from all shells, not only fish

--- private_dot_config/fish/config.fish
+++ private_dot_config/fish/config.fish
@@ -1,22 +1,6 @@
 if status is-interactive
 
-  # Note: use ~/.profile and ~/.xsessionrc instead for env variables that need to be globally available outside shell 
-
-  # First login: decrypt encrypted chezmoi files
-  if type -q chezmoi
-    if command -v gpg > /dev/null; and gpg --list-secret-keys > /dev/null 2>&1
-      if not test -f "$HOME/.config/fish/conf.d/secret.environment.fish"
-        set_color green
-        echo ">> First connection detected: Decrypting chezmoi secrets..."
-        set_color normal
-        chezmoi apply --include=encrypted
-        set_color green
-        echo ">> Secrets decrypted successfully."
-        set_color normal
-      end
-    end
-  end
-
+  # Note: use ~/.profile, ~/.environment and ~/.xsessionrc instead for env variables that need to be globally available outside shell 
   if test -d ~/.rd/bin
     set -gx --append fish_user_paths "$HOME/.rd/bin"
   end
